141dcbd97b6200eb4b2ae9f81a7005370dc0b6fc
sapsu <113932803+sapsu@users.noreply.github.com>
2023-01-08 18:08:12 +0100

    Updated css and html of weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,56 @@
 import React from "react";
 import "./Weather.css";
-import Forecast from "./Forecast";
 export default function Weather(props) {
   if (props.temperature) {
     return (
       <div className="container-fluid">
-        <div className="row text-center">
-          <div className="col-md-6 pt-2 pb-2">
-            <h2>{props.city}</h2>
+        <div className="row text-center m-2">
+          <h1>{props.city}</h1>
 
-            <h3>
-              <img src={props.icon} alt="weather icon" width="60" />{" "}
-              {props.temperature} | °F
-            </h3>
-            <h4>{props.description}</h4>
-            <h5>{props.humidity}</h5>
-            <h5>{props.wind}</h5>
+          <h2>
+            <img src={props.icon} alt="weather icon" width="60" />{" "}
+            {props.temperature} | °F
+          </h2>
+          <h3>{props.description}</h3>
+          <h5>{props.humidity}</h5>
+          <h5>{props.wind}</h5>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col">
+            <ul>
+              <li>Mon</li>
+              <li>13</li>
+              <li>⛅</li>
+            </ul>
           </div>
-          <div className="col-md-6 pt-2 pb-2" id="forecast">
-            <Forecast day="Monday" temperature={13} icon="⛅" />
-            <Forecast day="Tuesday" temperature={15} icon="⛅" />
-            <Forecast day="Wednesday" temperature={12} icon="⛅" />
-            <Forecast day="Thursday" temperature={17} icon="⛅" />
-            <Forecast day="Friday" temperature={14} icon="⛅" />
+          <div className="col">
+            <ul>
+              <li>Tue</li>
+              <li>15</li>
+              <li>⛅</li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Wed</li>
+              <li>14</li>
+              <li>⛅</li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Thu</li>
+              <li>13</li>
+              <li>⛅</li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Fri</li>
+              <li>13</li>
+              <li>⛅</li>
+            </ul>
           </div>
         </div>
       </div>

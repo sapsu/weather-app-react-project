21bf3ccc132d0e58e2b0c1284eaf8cbbfe72be98
sapsu <113932803+sapsu@users.noreply.github.com>
2023-01-09 14:07:07 +0100

    Added animated icons for forecast dummy data

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,13 +1,14 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo container-fluid">
       <div className="row text-center m-2">
         <h1>{props.data.city}</h1>
-        <h2>
+        <h2 id="time">
           <FormattedDate date={props.data.date} />
         </h2>
 
@@ -15,7 +16,7 @@ export default function WeatherInfo(props) {
           <WeatherIcon code={props.data.icon} />{" "}
           {Math.round(props.data.temperature)} °C | °F
         </h2>
-        <h3 className="text-capitalize">{props.data.description}</h3>
+        <h5 className="text-capitalize">{props.data.description}</h5>
         <h5>Humidity: {props.data.humidity} %</h5>
         <h5>Wind: {Math.round(props.data.wind)} km/h</h5>
       </div>
@@ -24,36 +25,71 @@ export default function WeatherInfo(props) {
         <div className="col">
           <ul>
             <li>Mon</li>
-            <li>13</li>
-            <li>⛅</li>
+            <li>13°C</li>
+            <li>
+              <ReactAnimatedWeather
+                icon="CLOUDY"
+                color="white"
+                size={20}
+                animate={true}
+              />
+            </li>
           </ul>
         </div>
         <div className="col">
           <ul>
             <li>Tue</li>
-            <li>15</li>
-            <li>⛅</li>
+            <li>15°C</li>
+            <li>
+              <ReactAnimatedWeather
+                icon="PARTLY_CLOUDY_DAY"
+                color="white"
+                size={20}
+                animate={true}
+              />
+            </li>
           </ul>
         </div>
         <div className="col">
           <ul>
             <li>Wed</li>
-            <li>14</li>
-            <li>⛅</li>
+            <li>14°C</li>
+            <li>
+              <ReactAnimatedWeather
+                icon="FOG"
+                color="white"
+                size={20}
+                animate={true}
+              />
+            </li>
           </ul>
         </div>
         <div className="col">
           <ul>
             <li>Thu</li>
-            <li>13</li>
-            <li>⛅</li>
+            <li>13°C</li>
+            <li>
+              <ReactAnimatedWeather
+                icon="PARTLY_CLOUDY_DAY"
+                color="white"
+                size={20}
+                animate={true}
+              />
+            </li>
           </ul>
         </div>
         <div className="col">
           <ul>
             <li>Fri</li>
-            <li>13</li>
-            <li>⛅</li>
+            <li>13°C</li>
+            <li>
+              <ReactAnimatedWeather
+                icon="RAIN"
+                color="white"
+                size={20}
+                animate={true}
+              />
+            </li>
           </ul>
         </div>
       </div>

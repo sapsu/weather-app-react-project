91db158288456fb5018fc6363e723ca0c6f96ed2
sapsu <113932803+sapsu@users.noreply.github.com>
2023-01-10 13:35:04 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,8 +24,9 @@ export default function WeatherForecast(props) {
                 <WeatherForecastDay data={dailyForecast} />
               </div>
             );
+          } else {
+            return null;
           }
-          return null;
         })}
       </div>
     );
